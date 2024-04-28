e1796920b93506a3a2cdc6a1ad193cc0d283c5bb
vvskchaitanya <vvskchaitanya@gmail.com>
2024-04-28 18:43:15 +0000

    updated token

--- ecommerce-api/src/main/java/com/vvsk/ecommerce/ecommerceapi/service/JwtTokenService.java
+++ ecommerce-api/src/main/java/com/vvsk/ecommerce/ecommerceapi/service/JwtTokenService.java
@@ -2,12 +2,10 @@ package com.vvsk.ecommerce.ecommerceapi.service;
 
 import java.util.Date;
 
-import javax.crypto.SecretKey;
-
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.Jwts.ENC;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtTokenService {
@@ -15,26 +13,12 @@ public class JwtTokenService {
     private static final String SECRET = "elzKUEbUJFd78m1oX46NFBLZJ38O0sk0";
     private static final long EXPIRATION_TIME = 864_000_000;
 
-    private static SecretKey SECRET_KEY =new SecretKey() {
-        @Override
-        public String getAlgorithm() {
-            return "RS256";
-        }
-        @Override
-        public byte[] getEncoded() {
-            return SECRET.getBytes();
-        }
-        @Override
-        public String getFormat() {
-            return "UFT-8";
-        }
-    };
     
     public String generateToken(String username) {
         return Jwts.builder()
-        .claim("subject", "username")
+        .claim("subject", username)
         .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-        .encryptWith(SECRET_KEY, ENC.A128CBC_HS256).compact();
+        .signWith(Keys.hmacShaKeyFor(SECRET.getBytes())).compact();
     }
 
 }
