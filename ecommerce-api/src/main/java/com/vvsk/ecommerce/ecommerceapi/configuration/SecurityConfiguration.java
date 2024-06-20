b63739f898e970f06ac1ac6d8832c91a54123206
vvskchaitanya <vvskchaitanya@gmail.com>
2024-06-20 19:11:21 +0000

    updated

--- ecommerce-api/src/main/java/com/vvsk/ecommerce/ecommerceapi/configuration/SecurityConfiguration.java
+++ ecommerce-api/src/main/java/com/vvsk/ecommerce/ecommerceapi/configuration/SecurityConfiguration.java
@@ -1,17 +1,18 @@
 package com.vvsk.ecommerce.ecommerceapi.configuration;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.vvsk.ecommerce.ecommerceapi.service.JwtTokenService;
 
 import jakarta.annotation.PostConstruct;
 
@@ -23,6 +24,9 @@ public class SecurityConfiguration{
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Value("app.public.endpoints")
+    String[] allowList;
 
     @PostConstruct()
     public void configure(){
@@ -40,6 +44,7 @@ public class SecurityConfiguration{
         http.addFilterAfter(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
         http.authorizeHttpRequests(req->{
             req.requestMatchers("/","/swagger-ui/**","/v3/**","/users/register","/authentication/login").permitAll()
+            .requestMatchers(allowList).permitAll()
             .anyRequest().authenticated();
         });
         return http.build();
